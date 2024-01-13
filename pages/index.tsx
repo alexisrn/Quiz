0780dee61f6f2a08218442664399992a7969c691
Raphael  Alexis do Nascimento <alexis.raphael95@gmail.com>
2024-01-13 14:43:18 -0300

    url-fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'https://quiz-sigma-jet.vercel.app/api'
+const BASE_URL = 'https://quiz-g8oj2vfly-alexisrn.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
